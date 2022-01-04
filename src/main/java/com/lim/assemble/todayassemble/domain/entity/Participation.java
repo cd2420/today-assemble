af212f7537fee92128c07244e27caaec9bc54ca8
imdaegeun <cd3818@naver.com>
2022-01-04 23:54:54 +0900

    1/4
    - 두개의 entity를 외래키로 가지는 entity.
    - mapping 어너테이션으로 테이블 생성 및 스키마 설정

---
+++ src/main/java/com/lim/assemble/todayassemble/domain/entity/Participation.java
@@ -0,0 +1,28 @@
+package com.lim.assemble.todayassemble.domain.entity;
+
+import com.lim.assemble.todayassemble.domain.entity.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users_participation_events")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participation extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "accounts_id", nullable = false)
+    private Accounts accounts;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "events_id", nullable = false)
+    private Events events;
+
+
+}
