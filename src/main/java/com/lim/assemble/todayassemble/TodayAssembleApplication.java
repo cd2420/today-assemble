762745e1a5f6e224961caad27cb15e09be7ca6a0
imdaegeun <cd3818@naver.com>
2022-01-04 21:50:35 +0900

    first commit

---
+++ src/main/java/com/lim/assemble/todayassemble/TodayAssembleApplication.java
@@ -0,0 +1,13 @@
+package com.lim.assemble.todayassemble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayAssembleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayAssembleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lim/assemble/todayassemble/TodayAssembleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim.assemble.todayassemble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayAssembleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
