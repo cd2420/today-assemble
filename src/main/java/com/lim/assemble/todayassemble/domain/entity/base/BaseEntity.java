74306fc6ec7012ffdd863f3477a74cb1388af8e2
imdaegeun <cd3818@naver.com>
2022-01-06 00:08:07 +0900

    1/6
    - baseEntity
      - Getter 추가

--- src/main/java/com/lim/assemble/todayassemble/domain/entity/base/BaseEntity.java
+++ src/main/java/com/lim/assemble/todayassemble/domain/entity/base/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.lim.assemble.todayassemble.domain.entity.base;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +10,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
