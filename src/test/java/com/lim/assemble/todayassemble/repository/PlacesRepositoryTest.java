a309827237f9f9a4994f3a758f56030c8366259a
imdaegeun <cd3818@naver.com>
2022-01-07 23:29:19 +0900

    1/7
    - PlacesImagesRepositoryTest 테스트 코드 구현

---
+++ src/test/java/com/lim/assemble/todayassemble/repository/PlacesRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.lim.assemble.todayassemble.repository;
+
+import com.lim.assemble.todayassemble.domain.entity.Places;
+import com.lim.assemble.todayassemble.entity.EntityFactory;
+import com.lim.assemble.todayassemble.entity.Entity_Type;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PlacesRepositoryTest {
+
+    @Autowired
+    private PlacesRepository placesRepository;
+
+    @Test
+    @DisplayName("기본 장소 저장")
+    @Transactional
+    public void givenBasicData_whenSaveRepository_thenReturnsBasicData(){
+        // given
+        Places places = (Places) EntityFactory.createEntity(Entity_Type.PLACES);
+
+        // when
+        placesRepository.save(places);
+
+        // then
+        List<Places> placesList = placesRepository.findAll();
+        assertEquals(places.getName(), placesList.get(0).getName());
+    }
+
+}
