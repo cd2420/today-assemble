b5e4cae172f8d0173867c1d15a2fdc4c3280008f
imdaegeun <cd3818@naver.com>
2022-01-07 22:52:31 +0900

    1/7
    - Test용 EntityFactory createEntity 부분 리팩터링

--- src/test/java/com/lim/assemble/todayassemble/repository/EntityFactory.java
+++ src/test/java/com/lim/assemble/todayassemble/repository/EntityFactory.java
@@ -16,44 +16,57 @@ public class EntityFactory {
     public static Object createEntity(Entity_Type entity_type) {
 
         if (entity_type == Entity_Type.ACCOUNTS) {
-            return Accounts.builder()
-                    .name("limdaegeun")
-                    .email("asdw@asdw.com")
-                    .password("adwdzcxasd")
-                    .accountType(AccountsType.CLIENT)
-                    .gender(Gender.MALE)
-                    .birth(LocalDateTime.now())
-                    .age(30)
-                    .emailVerified(true)
-                    .emailCheckToken("asdwdadwasdw")
-                    .build();
-
+            return createAccounts();
         } else if(entity_type == Entity_Type.ACCOUNTS_IMAGES) {
-            return AccountsImages.builder()
-                    .accounts((Accounts) createEntity(Entity_Type.ACCOUNTS))
-                    .image("path~~~~~~~~")
-                    .imagesType(ImagesType.MAIN)
-                    .build();
+            return createAccountsImages(createAccounts());
         } else if(entity_type == Entity_Type.EVENTS) {
-            Accounts accounts = (Accounts) createEntity(Entity_Type.ACCOUNTS);
-            Places places = (Places) createEntity(Entity_Type.PLACES);
-            return Events.builder()
-                    .name("스파이더맨 보러갈 모임")
-                    .host_email(accounts.getEmail())
-                    .description("영화 모임~~~~~~~~~~")
-                    .accounts(accounts)
-                    .places(places)
-                    .maxMembers(4)
-                    .likes(0)
-                    .eventsType(EventsType.OFFLINE)
-                    .build();
+            return createEvents(createAccounts(), createPlaces());
         } else {
-            return Places.builder()
-                    .name("홍대CGV")
-                    .address("서울 ~~ ~~ ~~")
-                    .latitude("12312932")
-                    .longtitude("112031123")
-                    .build();
+            return createPlaces();
         }
+    }
+
+    private static Places createPlaces() {
+        return Places.builder()
+                .name("홍대CGV")
+                .address("서울 ~~ ~~ ~~")
+                .latitude("12312932")
+                .longtitude("112031123")
+                .build();
+    }
+
+    private static Events createEvents(Accounts accounts, Places places) {
+        return Events.builder()
+                .name("스파이더맨 보러갈 모임")
+                .host_email(accounts.getEmail())
+                .description("영화 모임~~~~~~~~~~")
+                .accounts(accounts)
+                .places(places)
+                .maxMembers(4)
+                .likes(0)
+                .eventsType(EventsType.OFFLINE)
+                .build();
+    }
+
+    private static AccountsImages createAccountsImages(Accounts accounts) {
+        return AccountsImages.builder()
+                .accounts(accounts)
+                .image("path~~~~~~~~")
+                .imagesType(ImagesType.MAIN)
+                .build();
+    }
+
+    private static Accounts createAccounts() {
+        return Accounts.builder()
+                .name("limdaegeun")
+                .email("asdw@asdw.com")
+                .password("adwdzcxasd")
+                .accountType(AccountsType.CLIENT)
+                .gender(Gender.MALE)
+                .birth(LocalDateTime.now())
+                .age(30)
+                .emailVerified(true)
+                .emailCheckToken("asdwdadwasdw")
+                .build();
     }
 }
