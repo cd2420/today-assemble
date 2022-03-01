0ed9a7975bada0ea8a8025adb5baaba94c0ae2e2
imdaegeun <cd3818@naver.com>
2022-03-01 18:04:09 +0900

    3/1
    - 로그아웃시, 시큐어리티 콘텍스트 비우기

--- src/main/java/com/lim/assemble/todayassemble/main/MainController.java
+++ src/main/java/com/lim/assemble/todayassemble/main/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,7 @@ public class MainController {
     public ResponseEntity<Void> logout(
            HttpServletRequest request) {
         log.info("url: {}", request.getRequestURI());
+        SecurityContextHolder.clearContext();
         return  ResponseEntity.ok().build();
     }
 
