096b633f618aed267e6ae8571bff6a8aeb3ba16b
imdaegeun <cd3818@naver.com>
2022-03-01 16:39:50 +0900

    3/1
    - 이메일 인증후, react front 주소로 redirect 시켜줌

--- src/main/java/com/lim/assemble/todayassemble/email/controller/EmailController.java
+++ src/main/java/com/lim/assemble/todayassemble/email/controller/EmailController.java
@@ -1,23 +1,25 @@
 package com.lim.assemble.todayassemble.email.controller;
 
+import com.lim.assemble.todayassemble.common.property.AppProperties;
 import com.lim.assemble.todayassemble.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/email")
 public class EmailController {
 
     private final EmailService emailService;
+    private final AppProperties appProperties;
 
     @GetMapping("/check-email-token")
-    public ResponseEntity<Void> checkEmailToken(
+    public String checkEmailToken(
             @RequestParam String token
             , @RequestParam String email
             , HttpServletRequest request
@@ -28,7 +30,7 @@ public class EmailController {
                 , email
         );
         emailService.verify(email, token);
-        return ResponseEntity.ok().build();
+        return "redirect:" + appProperties.getFront_host();
     }
 
 }
