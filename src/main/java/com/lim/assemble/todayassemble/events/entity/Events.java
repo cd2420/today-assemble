c69359d8fe943e7772d119fb142eec3bec7f1a07
imdaegeun <cd3818@naver.com>
2022-07-31 18:09:26 +0900

    7/31 SpringBootTest에서 Events 생성 테스트할 때 영속성 문제 발생  >>> Request processing failed; nested exception is org.springframework.dao.InvalidDataAccessApiUsageException: org.hibernate.TransientPropertyValueException: Not-null property references a transient value - transient instance must be saved before current operation  : 원인 FK로 쓰는 객체 (Accounts)가 아직 저장이 안되어서 나는 오류.  저장이 되도록 영속성 전이 설정을 한다.

--- src/main/java/com/lim/assemble/todayassemble/events/entity/Events.java
+++ src/main/java/com/lim/assemble/todayassemble/events/entity/Events.java
@@ -31,7 +31,7 @@ public class Events extends BaseEntity implements Serializable {
     @Lob @Basic(fetch = FetchType.EAGER)
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "host_id", nullable = false)
     @JsonIgnore
     private Accounts accounts;
