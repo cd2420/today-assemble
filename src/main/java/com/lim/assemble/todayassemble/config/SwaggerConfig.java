a5ec5e0c7cba4bb6657a36ba560f4976f8137e5d
imdaegeun <cd3818@naver.com>
2022-08-07 22:43:09 +0900

    8/7 - Swagger 설정 : Swagger에서 JWT 인증 기능 사용가능하게 설정

--- src/main/java/com/lim/assemble/todayassemble/config/SwaggerConfig.java
+++ src/main/java/com/lim/assemble/todayassemble/config/SwaggerConfig.java
@@ -7,10 +7,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -25,19 +30,35 @@ public class SwaggerConfig {
     @Bean
     public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(List.of(securityContext()))
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
                 .apiInfo(swaggerInfo()).select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/v1/**"))
-//                .apis(RequestHandlerSelectors.basePackage("com.lim.assemble.todayassemble.accounts.controller"))
-//                .apis(RequestHandlerSelectors.basePackage("com.lim.assemble.todayassemble.email.controller"))
-//                .apis(RequestHandlerSelectors.basePackage("com.lim.assemble.todayassemble.events.controller"))
-//                .apis(RequestHandlerSelectors.basePackage("com.lim.assemble.todayassemble.main"))
                 .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false);
+                .useDefaultResponseMessages(false)
+                ;
+
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
 
     private Set<String> getConsumeContentTypes() {
         Set<String> consumes = new HashSet<>();
