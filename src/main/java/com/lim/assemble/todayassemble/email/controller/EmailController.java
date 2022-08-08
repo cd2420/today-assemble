73fdf16eb2559ad956f6447dd273f5e2d8c25608
imdaegeun <cd3818@naver.com>
2022-08-08 22:45:08 +0900

    8/8 - EmailController Swagger 설정

--- src/main/java/com/lim/assemble/todayassemble/email/controller/EmailController.java
+++ src/main/java/com/lim/assemble/todayassemble/email/controller/EmailController.java
@@ -6,6 +6,8 @@ import com.lim.assemble.todayassemble.common.type.EmailsType;
 import com.lim.assemble.todayassemble.email.service.EmailService;
 import com.lim.assemble.todayassemble.exception.ErrorCode;
 import com.lim.assemble.todayassemble.exception.TodayAssembleException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +26,11 @@ public class EmailController {
     private final EmailService emailService;
     private final AppProperties appProperties;
 
+    @ApiOperation(value = "계정 인증 토큰 체크")
     @GetMapping("/check-email-token")
     public ResponseEntity<AccountsDto> checkEmailToken(
-            @RequestParam String token
-            , @RequestParam String email
+            @ApiParam(value = "토큰") @RequestParam String token
+            , @ApiParam(value = "email")  @RequestParam String email
             , HttpServletRequest request
             , HttpServletResponse response
     ) {
@@ -40,10 +43,11 @@ public class EmailController {
         return ResponseEntity.ok(emailService.emailToken(email, token, EmailsType.VERIFY, response));
     }
 
+    @ApiOperation(value = "이메일 로그인 토큰 체크")
     @GetMapping("/login-email-token")
     public ResponseEntity<AccountsDto> loginEmailToken(
-            @RequestParam String token
-            , @RequestParam String email
+            @ApiParam(value = "토큰") @RequestParam String token
+            , @ApiParam(value = "email")  @RequestParam String email
             , HttpServletRequest request
             , HttpServletResponse response
     ) {
